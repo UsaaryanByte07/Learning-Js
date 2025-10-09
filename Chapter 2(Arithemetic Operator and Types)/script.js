8d6d93f6cd75b6ab6cf333c91e3dba01863525ad
UsaaryanByte07 <usaaryan79@gmail.com>
2025-10-09 13:24:42 +0530

    Added Chapter 2

---
+++ Chapter 2(Arithemetic Operator and Types)/script.js
@@ -0,0 +1,19 @@
+console.log('Hello' + ' ' + 4);
+console.log(10/3); //Floating point Issue
+console.log(Math.round(10/3));
+console.log(10%3);
+console.log(10*3);
+console.log("Hellooo");
+console.log(`Hellooo`);
+console.log(true);
+console.log(10 + 3 - 10);
+console.log("Hello" + true);
+console.log("KG" + 2 + 10);
+console.log(2 + 10 + "KG");
+console.log(null);
+console.log(undefined);
+console.log(typeof(12));
+console.log(typeof(on));
+console.log(typeof(4 + 'hello'));
+console.log(typeof('45' + true));
+console.log(typeof('12'));
