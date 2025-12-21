6e56ba6e2910b721cfc3c863eb2292b135918703
UsaaryanByte07 <usaaryan79@gmail.com>
2025-12-21 18:02:12 +0530

    Added Chapter 9

---
+++ Chapter 9(Arrays)/array.js
@@ -0,0 +1,38 @@
+let student1 = {
+  firstName: 'Ram',
+  lastName: 'Kumar',
+  age: 19,
+  rollNo: 101,
+};
+let student2 = {
+  firstName: 'Ram',
+  lastName: 'Kumar',
+  age: 19,
+  rollNo: 101,
+};
+
+let student3 = {
+  firstName: 'Ram',
+  lastName: 'Kumar',
+  age: 19,
+  rollNo: 101,
+};
+
+
+let prime1 = 2;
+let prime2 = 3;
+let prime3 = 5;
+
+let primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23];
+console.log(primeNumbers[0]);
+console.log(primeNumbers[1]);
+console.log(primeNumbers[2]);
+
+let myIndex = 2;
+console.log('Num:', primeNumbers[myIndex]);
+
+let i = 0;
+while (i < primeNumbers.length) {
+  console.log('MyNum:', primeNumbers[i]);
+  i++;
+}
